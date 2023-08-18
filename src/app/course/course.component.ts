d7742e4a8e8dfe7e5621ed8a6a208544c3d8d634
Charl913 <charlkruger61@gmail.com>
2023-08-18 09:13:55 +0200

    Build a RxJs Custom Operator

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -13,9 +13,10 @@ import {
     withLatestFrom,
     concatAll, shareReplay
 } from 'rxjs/operators';
-import { merge, fromEvent, Observable, concat } from 'rxjs';
+import { merge, fromEvent, Observable, concat, forkJoin } from 'rxjs';
 import { Lesson } from '../model/lesson';
 import { createHttpObservable } from '../../util';
+import { RxJsLoggingLevel, debug, setRxJsLoggingLevel } from '../common/debug';
 
 
 @Component({
@@ -42,19 +43,38 @@ export class CourseComponent implements OnInit, AfterViewInit {
         this.courseId = this.route.snapshot.params['id'];
 
 
-        this.course$ = createHttpObservable(`/api/courses/${this.courseId}`);
+        const course$ = createHttpObservable(`/api/courses/${this.courseId}`)
+            .pipe(
+                debug(RxJsLoggingLevel.INFO, 'course value')
+            );
+
+        setRxJsLoggingLevel(RxJsLoggingLevel.TRACE);
+
+        const lessons$ = this.loadLessons();
+
+        // forkJoin(course$, lessons$)
+        //     .pipe(
+        //         tap(([course, lesson]) => {
+        //             console.log('course', course);
+        //             console.log('lesson', lesson);
+        //         })
+        //     )
+        //     .subscribe()
     }
 
     ngAfterViewInit() {
 
-        this.lessons$ = fromEvent<any>(this.input.nativeElement, 'keyup')
-        .pipe(
-            map(event => event.target.value),
-            startWith(''),
-            debounceTime(400),
-            distinctUntilChanged(),
-            switchMap(search => this.loadLessons(search))
-        );
+        fromEvent<any>(this.input.nativeElement, 'keyup')
+            .pipe(
+                map(event => event.target.value),
+                startWith(''),
+                debug(RxJsLoggingLevel.TRACE, 'search'),
+                debounceTime(400),
+                distinctUntilChanged(),
+                switchMap(search => this.loadLessons(search)),
+                debug(RxJsLoggingLevel.DEBUG, 'lessons value')
+            )
+            .subscribe(console.log)
     }
 
     loadLessons(search = ''): Observable<Lesson[]> {
